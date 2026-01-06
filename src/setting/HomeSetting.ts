3586252c3a966ea4d911c36e7c5d7fde35d43616
GEN-d233 <taoshan67523@163.com>
2026-01-07 02:19:46 +0800

    Update HomeSetting.ts

--- src/setting/HomeSetting.ts
+++ src/setting/HomeSetting.ts
@@ -1,5 +1,5 @@
 //主标题
-export const mainTitle = "0xygen Blog";
+export const mainTitle = "GEN Blog";
 //主题色装饰字
 export const mainTitleBlueDecoration = ".";
 //副标题
@@ -8,6 +8,6 @@ export const subTitle = "A Front-End ";
 export const subTitleBlueDecoration = "Developer";
 //打字机动画轮播数组
 export const TypewriterTexts = [
-  "悟以往之不谏，知来者之可追",
-  "JavaScript是最好的语言!",
+  "未来很美，放手去追",
+  "Python是最好的语言!",
 ];
