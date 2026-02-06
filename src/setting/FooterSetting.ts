67b214504474e2b00afbf2dd439680cb17898bfc
GEN-d233 <taoshan67523@163.com>
2026-02-07 02:11:48 +0800

    Update FooterSetting.ts

--- src/setting/FooterSetting.ts
+++ src/setting/FooterSetting.ts
@@ -4,7 +4,7 @@
 export const policeBeian = "京公网安备6666666号";
 export const ICP = "京ICP备12345678号-1"; // ICP备案号 (示例)
 export const year = "2025"; // 年份
-export const name = "G3ng@r"; // 名字
+export const name = "G3ng4r"; // 名字
 export const aWord = "Just try my best"; // 自定义一句话
 
 //ICP 备案号链接配置 根据工信部要求，ICP 备案号应该链接到备案查询网站
